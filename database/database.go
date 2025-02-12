be95a90b6f0d849044d803acc7a190695d258a8c
Kacper Gajko <kacper.gajko1@icloud.com>
2025-02-12 22:09:31 +0100

    Connect to database

---
+++ database/database.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectToDB() {
+	var err error
+
+	// Database connect vars
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
+
+	// Connect to db
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Println("Failed to connect to database!")
+	}
+	if DB != nil {
+		log.Println("Connected to database!")
+	}
+
+}
