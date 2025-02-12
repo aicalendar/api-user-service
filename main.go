6bfdb8017ed4a4ae9b3ba1d255989c3773def227
Kacper Gajko <kacper.gajko1@icloud.com>
2025-02-12 22:13:37 +0100

    Gorm setup (#14)
    * installed gorm
    
    * Connect to database

--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"api-user-service/database"
 	"api-user-service/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	// Default gin engine instance
 	r := gin.Default()
 
+	// Connect to db
+	database.ConnectToDB()
+
+	// Setup all api endpoints
 	SetupRoutes(r)
 
+	// Run gin server
 	r.Run(":80")
 }
 
