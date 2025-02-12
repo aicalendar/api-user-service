20e3d999852f841670ea931016cb9d03d87e8a91
Kacper Gajko <kacper.gajko1@icloud.com>
2025-02-12 23:46:21 +0100

    added comments

--- routes/registerUser.go
+++ routes/registerUser.go
@@ -19,12 +19,14 @@ type User struct {
 func RegisterUser(c *gin.Context) {
 	var newJSONUser User
 
+	// Binds request to newJSONUser variable
 	if err := c.BindJSON(&newJSONUser); err != nil {
 		log.Println("Failed to bind JSON")
 		c.JSON(http.StatusBadRequest, "Failed to bind JSON")
 		return
 	}
 
+	// User to insert into database
 	newDbUser := User{
 		ID:        uuid.New().String(),
 		Name:      newJSONUser.Name,
@@ -32,13 +34,17 @@ func RegisterUser(c *gin.Context) {
 		CreatedAt: time.Now(),
 	}
 
+	// Trying to insert into database
 	dbCreateResult := database.DB.Create(&newDbUser)
+
+	// Inserting error
 	if dbCreateResult.Error != nil {
 		log.Println("Error occurred while inserting to database!")
 		c.IndentedJSON(http.StatusInternalServerError, "Error occurred while inserting to database!")
 		return
 	}
 
+	// Insert success
 	if dbCreateResult.RowsAffected > 0 {
 		log.Println("Successfully register new user!")
 		c.IndentedJSON(http.StatusCreated, newDbUser)
