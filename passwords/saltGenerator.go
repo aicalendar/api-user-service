048405cab63fedb2ec1c160db19fbaa676e0ef8f
Kacper Gajko <kacper.gajko1@icloud.com>
2025-02-13 01:29:26 +0100

    logging error on salt generator

--- passwords/saltGenerator.go
+++ passwords/saltGenerator.go
@@ -2,14 +2,21 @@ package passwords
 
 import (
 	"crypto/rand"
+	"github.com/rs/zerolog/log"
 )
 
 // Generate a random salt
-func GenerateSalt(size int) ([]byte, error) {
+func GenerateSalt(size int) []byte {
+
+	// Generate random bytes
 	salt := make([]byte, size)
-	_, err := rand.Read(salt) // Secure random bytes
+	_, err := rand.Read(salt)
+
+	// Log on error
 	if err != nil {
-		return nil, err
+		log.Error().Msg("Failed reading random salt into bytes!")
+		return nil
 	}
-	return salt, nil
+
+	return salt
 }
