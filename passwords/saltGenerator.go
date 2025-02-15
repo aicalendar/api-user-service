fcaa0a0a291aba428a0b55b27c0a566a97842cea
Kacper Gajko <kacper.gajko1@icloud.com>
2025-02-15 06:02:33 +0100

    salt size < 0 error handle

--- passwords/saltGenerator.go
+++ passwords/saltGenerator.go
@@ -2,10 +2,16 @@ package passwords
 
 import (
 	"crypto/rand"
+	"errors"
 )
 
 // Generate a random salt
 func GenerateSalt(size int) ([]byte, error) {
+
+	// Handle error size smaller than 1
+	if size < 0 {
+		return nil, errors.New("Canno't generate salt with byte size smaller than 1!")
+	}
 
 	// Generate random bytes
 	salt := make([]byte, size)
