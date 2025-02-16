4ae59e324975cf3484d99f389e93258f7ef500af
Kacper Gajko <kacper.gajko1@icloud.com>
2025-02-17 00:59:31 +0100

    check if string has enough letters

---
+++ utils/hasEnoughLetters.go
@@ -0,0 +1,14 @@
+package utils
+
+import (
+	"unicode/utf8"
+)
+
+func HasEnoughLetters(string string, count int) bool {
+	length := utf8.RuneCountInString(string)
+
+	if length < count {
+		return false
+	}
+	return true
+}
